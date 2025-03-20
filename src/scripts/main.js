d413d87d42e7823c7db25c36448f0b51b983970f
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-20 15:12:08 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bigImg = document.getElementById('largeImg');
+const smallPhotos = document.querySelectorAll('.list-item');
+
+smallPhotos.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const pictures = e.target.closest('img');
+
+    e.preventDefault();
+
+    if (pictures) {
+      const img = pictures.src;
+
+      bigImg.src = img;
+    }
+  });
+});
+
+// console.log(smallPhotos)
