2f30501a83de7edda9871e3b183eafb0a951314a
Serhii Mamchur <kovaniysvit@gmail.com>
2025-03-21 08:23:11 +0100

    make build

---
+++ index.801e39c0.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.querySelector(".gallery__list").addEventListener("click",function(t){var l=t.target.closest(".list-item__link");l&&(t.preventDefault(),e.src=l.href)});
+//# sourceMappingURL=index.801e39c0.js.map
